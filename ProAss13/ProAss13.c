da509f6a4c514b5121dfb4568004cfee934242d0
dlghwns <dlghw@DESKTOP-9EFHI8D>
2025-05-02 22:26:31 +0900

    .

--- ProAss13/ProAss13.c
+++ ProAss13/ProAss13.c
@@ -16,15 +16,69 @@
 #include <stdio.h>
 
 
+
+
+
+
+ /*
+   * 함수명 : is_leap_year()
+   * 기능(책임) : 연도를 인자로 전달받아 윤년인지 검사함.
+   * 	윤년이면 연도를 출력하고, 아니면 아무것도 하지 않음.
+   *
+   *
+   * 반환 : int : 윤년이면 연도, 아니면 아무것도 하지 않음
+   */
+int is_leap_year(int year)
+{
+	return ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0));
+}
+
+/*
+* 함수명 : get_days_of_month()
+* 기능(책임) : 연도를 전달받아 그 달이 며칠까지 있는지 출력..
+*
+* 반환 : 없음
+*/
+void get_days_of_month(int year)
+{
+	if (year < 13)
+	{
+		printf("연도가 올바르지 않습니다\n");
+	}
+
+	if (is_leap_year(year))
+	{
+		//윤년 : 2월달은 29일까지.
+		int month[] = { 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+		for (int i = 0; i < 12; i++)
+		{
+			printf("%d월: %d일  ", i + 1, month[i]);
+		}
+
+	}
+	else
+	{
+		int month[] = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+		for (int i = 0; i < 12; i++)
+		{
+			printf("%d월: %d일  ", i + 1, month[i]);
+		}
+	}
+}
+
  /*
  * 함수명 : solve()
- * 기능(책임) :
+ * 기능(책임) : 연도를 입력받아 get_days_of_month 함수를 호출함.
  *
  * 반환 : 없음
  */
 void solve()
 {
-
+	int year = 0;
+	printf("연도?");
+	scanf("%d", &year);
+	printf("[ %d년 ]\n", year);
+	get_days_of_month(year);
 }
 
 
